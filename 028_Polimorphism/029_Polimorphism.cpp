26db8ad90e1188239dcb10c1f43ce468f07e4f3b
Orach Max <alingman19@gmail.com>
2025-10-22 18:12:52 +0300

    Add classes A, B, C to demonstrate polymorphism
    Added class `A` with a `func()` method that prints a message.
    Introduced classes `B` and `C`, inheriting from `A`, each
    overriding the `func()` method with their own implementation.
    Updated the `main()` function to create objects of `A`, `B`,
    and `C` and call their respective `func()` methods, showcasing
    polymorphism in action.

--- 028_Polimorphism/029_Polimorphism.cpp
+++ 028_Polimorphism/029_Polimorphism.cpp
@@ -7,10 +7,39 @@ using namespace std;
 // працювати по різному, в залежності від ситуації
 // в момент виконання програми.
 
+class A {
+public:
+	void func() {
+		cout << "Class A func()" << endl;
+	}
+};
+
+class B : public A {
+public:
+	void func() {
+		cout << "Class B func()" << endl;
+	}
+};
+
+class C : public A {
+public:
+	void func() {
+		cout << "Class C func()" << endl;
+	}
+};
 
 int main()
 {
 	srand(time(0));
+
+	A a;
+	a.func();
+
+	B b;
+	b.func();
+
+	C c;
+	c.func();
 
 
 
